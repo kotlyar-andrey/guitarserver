e3303a83da5f4e3d10849f93c3448b6000626123
Andrey Kotlyar <kotlyar562@gmail.com>
2022-07-03 22:22:07 +0300

    remove unusable things

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,9 @@
-from django.http import Http404
-from rest_framework import generics, status
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .models import User
 from .serializers import UserSerializer, RegisterSerializer
 
 
